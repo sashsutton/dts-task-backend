b532abab157be534785355a7d00c44370c38652f
sashasutton <sasha.sutton@etu.univ-amu.fr>
2025-11-25 00:25:15 +0100

    connection with mongodb done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import {connectDB} from './utils/db.js';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+    connectDB();
 })
